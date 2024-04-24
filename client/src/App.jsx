9951bbd6e4440523f0299efd985cca26625e9891
Erjon Asllani <erjonasllani999@gmail.com>
2024-04-24 11:59:22 +0200

    Suggested commit message: "Refactor App component and main.jsx file formatting"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,41 +1,78 @@
-import React from 'react'
-import { Link, Route, Routes } from 'react-router-dom'
-import Home from 'pages/home'
-import Login from 'pages/login'
-import Users from 'pages/users' 
-import User from 'pages/users/user'
-import RequireAuth from 'components/auth/RequireAuth'
-import PersistLogin from 'components/auth/PersistLogin'
-import { Unauthorized } from 'components/auth/Unauthorized'
-import NavBar  from 'components/navbar'
-import { useAuth } from 'hooks/useAuth'
-import { NotFound } from 'components/NotFound'
-import './App.css'
+import React from 'react';
+import { Link, Route, Routes } from 'react-router-dom';
+import Home from 'pages/home';
+import Login from 'pages/login';
+import Users from 'pages/users';
+import User from 'pages/users/user';
+import RequireAuth from 'components/auth/RequireAuth';
+import PersistLogin from 'components/auth/PersistLogin';
+import { Unauthorized } from 'components/auth/Unauthorized';
+import NavBar from 'components/navbar';
+import { useAuth } from 'hooks/useAuth';
+import { NotFound } from 'components/NotFound';
+import './App.css';
 
 function App() {
-    const { auth } = useAuth();
-    const { user, userType } = auth;
+	const { auth } = useAuth();
+	const { user, userType } = auth;
 
-    return (
-        <>
-            <Routes>
-                <Route path='/login' element={<Login />} />
-                <Route element={<PersistLogin />} >
-                    <Route element={<NavBar />}>
-                        <Route element={<RequireAuth />} >
-                            <Route path='/' element={<Home user={user} userType={userType}/>} />
-                            <Route path='/users/:id' element={<User />} />
-                        </Route>
-                        <Route element={<RequireAuth allowedUserTypes={['ADMIN']}/>} >
-                            <Route path='/users' element={<Users />} />
-                        </Route>
-                        <Route path='/unauthorized' element={<Unauthorized />} />
-                    </Route>
-                </Route>
-                <Route path='*' element={<NotFound />} />
-            </Routes>
-        </>
-    )
+	return (
+		<>
+			<Routes>
+				<Route
+					path='/login'
+					element={<Login />}
+				/>
+				<Route element={<PersistLogin />}>
+					<Route
+						path='/*'
+						element={
+							<NavBar
+								userType={userType}
+								content={
+									<Routes>
+										<Route element={<RequireAuth />}>
+											<Route
+												index
+												element={
+													<Home
+														user={user}
+														userType={userType}
+													/>
+												}
+											/>
+											{AdminRoutes.map((route, i) => (
+												<Route
+													key={i}
+													path={route.path}
+													element={route.element}
+												/>
+											))}
+										</Route>
+									</Routes>
+								}
+							/>
+						}
+					>
+						<Route element={<RequireAuth allowedUserTypes={['ADMIN']} />}>
+							<Route
+								path='users'
+								element={<UsersPage />}
+							/>
+						</Route>
+						<Route
+							path='unauthorized'
+							element={<Unauthorized />}
+						/>
+					</Route>
+				</Route>
+				<Route
+					path='*'
+					element={<NotFound />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
 export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -9,9 +9,11 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter>
             <AuthProvider>
+				<I18nextProvider i18n={i18n}>
                 <Routes>
                     <Route path="/*" element={<App />} />
                 </Routes>
+				</I18nextProvider>
             </AuthProvider>
         </BrowserRouter>
     </React.StrictMode>,
