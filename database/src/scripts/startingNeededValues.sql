10a5b5986f47d5be4c883a7e7a5cdf7cd09b33f2
leartbllacaku <leartbllacaku11@gmail.com>
2024-05-15 11:24:44 +0200

    Update startingNeededValues.sql with school and user data

--- database/src/scripts/startingNeededValues.sql
+++ database/src/scripts/startingNeededValues.sql
@@ -32,3 +32,14 @@ SELECT role.id, capabilities.id
 FROM role, capabilities
 WHERE role.name = 'STUDENT'
 AND capabilities.name IN ('notifications', 'create-notification', 'list-material', 'add-material');
+
+INSERT INTO school (id, name, address, schooldomain) VALUES
+(1, 'School 1', 'Address 1', 'school1.com'),
+(2, 'School 2', 'Address 2', 'school2.com'),
+(3, 'School 3', 'Address 3', 'school3.com');
+
+INSERT INTO "User" (id, username, email, password, firstname, lastname, roleid, schoolid) VALUES
+(1, 'admin', 'admin@example.com' , '$2a$10$LI/rTpkcz8X8IhqlBYZo2.qXNK1c/7F8DwEV2H8bPqP7lxJUfeeZq', 'Admin', 'Admin', 1, 1),
+(2, 'teacher', 'teacher@example.com', '$2a$10$xh/azIq4XnVwh6YevEJb0OCDuj9.DpyUTY67l0hxGBY7oe7qMku0y', 'Teacher', 'Teacher', 2, 1),
+(3, 'student', 'student@example.com', '$2a$10$ulItq0ktCHmx3TLtP3gFkezcLNqF5dBEAi9nRsbesE8ggqtbiTC1a', 'Student', 'Student', 3, 1),
+(4, 'parent', 'parent@example.com', '$2a$10$ZiZtqW1tReR.T6n2PRwxZuOGD2ifOtWSCak6ytW0g6nL7YHtojgZO', 'Parent', 'Parent', 4, 1);
