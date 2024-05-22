12d4263a89f637465b34c8b306fd44e4c434d5f6
Erjon Asllani <erjonasllani999@gmail.com>
2024-05-22 02:43:45 +0200

    Update classId variable name in updateUser route

--- server/src/routes/users/updateUser.js
+++ server/src/routes/users/updateUser.js
@@ -7,7 +7,7 @@ const updateUserRouter = Router();
 updateUserRouter.put('/admin-update/:id', checkRole(null, ['ADMIN', 'TEACHER']), (req, res) => {
     const id = req.params.id;
     const request_for = req.params.request_for;
-    const { email, password, firstName, lastName, role, gender, phoneNumber, educationLevel, experienceYears, teachingSpecialization, personalNumber, birthday, parentId, class } = req.body;
+    const { email, password, firstName, lastName, role, gender, phoneNumber, educationLevel, experienceYears, teachingSpecialization, personalNumber, birthday, parentId, classId } = req.body;
     if(request_for === 'ADMIN'){
       db.query('SELECT updateAdminUser($1, $2, $3, $4, $5, $6)', [id, firstName, lastName, email, role, password], (err, queryRes) => {
           if (err) {
