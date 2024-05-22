22c2dc98ec3b5c9846a1fe4711db406e05fdce0f
vullnetgervalla <vullneti2003@gmail.com>
2024-05-22 20:50:36 +0200

    Fixed formatting

--- client/src/pages/admin/createAdmin.jsx
+++ client/src/pages/admin/createAdmin.jsx
@@ -6,126 +6,126 @@ import { useTranslation } from 'react-i18next';
 import { useAxiosPrivate } from 'hooks/useAxiosPrivate';
 
 const formItemLayout = {
-  labelCol: {
-    xs: {
-      span: 24,
+    labelCol: {
+        xs: {
+            span: 24,
+        },
+        sm: {
+            span: 24,
+        },
     },
-    sm: {
-      span: 24,
+    wrapperCol: {
+        xs: {
+            span: 24,
+        },
+        sm: {
+            span: 24,
+        },
     },
-  },
-  wrapperCol: {
-    xs: {
-      span: 24,
-    },
-    sm: {
-      span: 24,
-    },
-  },
 };
 
 function CreateAdmin() {
-  const { t } = useTranslation();
-  const axios = useAxiosPrivate();
-  const handleSubmit = async (values) => {
-    try {
-      const res = await axios.post('/users/admin', values);
-      message.success(t('createdAdmin'));
-    } catch (e) {
-      message.error(t('notCreatedAdmin'));
-    }
-  };
-  return (
-    <div style={{ textAlign: 'center', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
-      <Form
-        {...formItemLayout}
-        variant='filled'
-        style={{
-          minWidth: 700,
-          border: '1px solid #E5E7EB',
-          boxShadow: '3px 3px 10px #eee',
-          borderRadius: 20,
-          padding: 30,
-          backgroundColor: '#fff',
-        }}
-        onFinish={handleSubmit}
-      >
-        <Form.Item
-          label={t('name')}
-          labelAlign='left'
-          name='firstname'
-          rules={[
-            {
-              required: true,
-              message: t('enterName'),
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+    const { t } = useTranslation();
+    const axios = useAxiosPrivate();
+    const handleSubmit = async (values) => {
+        try {
+            const res = await axios.post('/users/admin', values);
+            message.success(t('createdAdmin'));
+        } catch (e) {
+            message.error(t('notCreatedAdmin'));
+        }
+    };
+    return (
+        <div style={{ textAlign: 'center', display: 'flex', alignItems: 'center', flexDirection: 'column' }}>
+            <Form
+                {...formItemLayout}
+                variant='filled'
+                style={{
+                    minWidth: 700,
+                    border: '1px solid #E5E7EB',
+                    boxShadow: '3px 3px 10px #eee',
+                    borderRadius: 20,
+                    padding: 30,
+                    backgroundColor: '#fff',
+                }}
+                onFinish={handleSubmit}
+            >
+                <Form.Item
+                    label={t('name')}
+                    labelAlign='left'
+                    name='firstname'
+                    rules={[
+                        {
+                            required: true,
+                            message: t('enterName'),
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-        <Form.Item
-          label={t('lastname')}
-          labelAlign='left'
-          name='lastname'
-          rules={[
-            {
-              required: true,
-              message: t('enterLastName'),
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+                <Form.Item
+                    label={t('lastname')}
+                    labelAlign='left'
+                    name='lastname'
+                    rules={[
+                        {
+                            required: true,
+                            message: t('enterLastName'),
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-        <Form.Item
-          label={t('email')}
-          labelAlign='left'
-          name='email'
-          rules={[
-            {
-              required: true,
-              message: t('enterMail'),
-            },
-            {
-              type: 'email',
-              message: t('invalidEmail'),
-            },
-          ]}
-        >
-          <Input />
-        </Form.Item>
+                <Form.Item
+                    label={t('email')}
+                    labelAlign='left'
+                    name='email'
+                    rules={[
+                        {
+                            required: true,
+                            message: t('enterMail'),
+                        },
+                        {
+                            type: 'email',
+                            message: t('invalidEmail'),
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
 
-        <Form.Item
-          label={t('password')}
-          labelAlign='left'
-          name='password'
-          rules={[
-            {
-              required: true,
-              message: t('enterPass'),
-            },
-          ]}
-        >
-          <Password />
-        </Form.Item>
+                <Form.Item
+                    label={t('password')}
+                    labelAlign='left'
+                    name='password'
+                    rules={[
+                        {
+                            required: true,
+                            message: t('enterPass'),
+                        },
+                    ]}
+                >
+                    <Password />
+                </Form.Item>
 
-        <Form.Item
-          wrapperCol={{
-            offset: 0,
-            span: 24,
-          }}
-        >
-          <Button
-            type='primary'
-            htmlType='submit'
-            style={{ marginTop: '1.5em', height: '3em' }}
-          >
-            {t('create-admin')}
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
-  );
+                <Form.Item
+                    wrapperCol={{
+                        offset: 0,
+                        span: 24,
+                    }}
+                >
+                    <Button
+                        type='primary'
+                        htmlType='submit'
+                        style={{ marginTop: '1.5em', height: '3em' }}
+                    >
+                        {t('create-admin')}
+                    </Button>
+                </Form.Item>
+            </Form>
+        </div>
+    );
 }
 export default CreateAdmin;
