d697a8766358c849e6ff41591d87e3aba078a850
Erjon Asllani <erjonasllani999@gmail.com>
2024-05-23 06:38:28 +0200

    Improve form layout in CreateTeacher component

--- client/src/pages/admin/createTeacher.jsx
+++ client/src/pages/admin/createTeacher.jsx
@@ -22,22 +22,6 @@ const formItemLayout = {
             span: 24,
         },
     },
-    labelCol: {
-        xs: {
-            span: 24,
-        },
-        sm: {
-            span: 24,
-        },
-    },
-    wrapperCol: {
-        xs: {
-            span: 24,
-        },
-        sm: {
-            span: 24,
-        },
-    },
 };
 
 function CreateTeacher() {
